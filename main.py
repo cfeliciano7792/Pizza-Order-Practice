10032d87908250ef5053cd5432986122dfe595b1
Christian F <96458958+cfeliciano7792@users.noreply.github.com>
2022-01-10 16:17:43 +0000

    Added Alternate Solution to coding challenge

--- main.py
+++ main.py
@@ -41,3 +41,25 @@ else:
 
 
 
+
+
+#Alternate Solution
+#bill = 0
+
+#if size == "S":
+  #bill += 15
+#elif size == "M":
+  #bill += 20
+#else:
+  #bill += 25
+
+#if add_pepperoni == "Y":
+  #if size == "S":
+   # bill += 2
+  #else:
+   # bill += 3
+    
+#if extra_cheese == "Y":
+ # bill += 1
+  
+#print(f"Your final bill is: ${bill}.")
